3bb5943ba3f255ec892b0f4307c96cbb6cd71c31
sonytxl <58774013+sonytxl@users.noreply.github.com>
2022-06-04 15:22:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' feeds.conf.default
+
