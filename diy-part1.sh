52be7b652b2cdbbf36e2bee3ca61609aa159c9a5
sonytxl <58774013+sonytxl@users.noreply.github.com>
2025-01-05 20:53:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,11 @@
 # Add a feed source
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+# echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+# echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+# echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
