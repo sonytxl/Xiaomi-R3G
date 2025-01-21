b1b0322226e72fb07ad8b00833d94e5f47ee7e61
sonytxl <58774013+sonytxl@users.noreply.github.com>
2025-01-21 13:09:17 +0800

    Update 闭源wifi.sh

--- 闭源wifi.sh
+++ 闭源wifi.sh
@@ -1,26 +1,14 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+# echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
-# ssr-plus+
-#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-
-# passwall
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#git clone  -b mt7915_mtwifi --single-branch https://github.com/padavanonly/immortalwrt
